7c9d6726a0542424ccef7e791e544216f1e8ea43
guaka <guaka@guaka.org>
2026-09-22 20:07:27 +0100

    Fill verified publication dates for Stringer stories (#4)
    * Add verified dates for Stringer stories
    
    * Add verified No fuimos Dinamarca date
    
    * Add Galamsey Economy publication date
    
    ---------
    
    Co-authored-by: Stringer Contributor <ingestr@users.noreply.github.com>

--- scripts/verified-publications.mjs
+++ scripts/verified-publications.mjs
@@ -17,6 +17,54 @@ export const VERIFIED_PUBLICATIONS = new Map([
     publicationDate: '2025-05-01',
     evidence: 'Publisher article header displays Jueves, 1 de mayo de 2025.',
   }],
+  ['https://www.thenewhumanitarian.org/first-person/2025/11/05/exile-i-watched-el-fasher-fall-and-my-family-fight-survive', {
+    publicationDate: '2025-11-05',
+    evidence: 'Publisher article header displays 5 November 2025.',
+  }],
+  ['https://www.thenewhumanitarian.org/opinion/2015/11/17/what-talks-burundi-should-look', {
+    publicationDate: '2015-11-17',
+    evidence: 'Publisher article header displays 17 November 2015.',
+  }],
+  ['https://www.bitacoraec.com/post/tr%C3%A1fico-de-iguanas-de-gal%C3%A1pagos-un-a%C3%B1o-de-silencio-del-ministerio-del-ambiente', {
+    publicationDate: '2023-04-25',
+    evidence: 'Publisher article header displays 25 April 2023.',
+  }],
+  ['https://www.premiumtimesng.com/regional/nwest/773640-investigation-behind-closed-doors-the-dark-reality-of-child-labour-in-a-northern-states-quranic-schools.html', {
+    publicationDate: '2025-02-12',
+    evidence: 'Publisher article page displays 12 February 2025.',
+  }],
+  ['https://www.aljazeera.com/video/people-power/2025/7/31/death-in-the-park', {
+    publicationDate: '2025-07-31',
+    evidence: 'Publisher video page displays 31 Jul 2025.',
+  }],
+  ['https://elestimulo.com/de-interes/2025-03-17/venezolano-en-carcel-de-el-salvador-francisco/', {
+    publicationDate: '2025-03-17',
+    evidence: 'Publisher article header displays 17 March 2025; later update is dated 19 March.',
+  }],
+  ['https://unbiasthenews.org/syria-war-poisoned-soil/', {
+    publicationDate: '2022-03-23',
+    evidence: 'Publisher article page displays 23 March 2022.',
+  }],
+  ['https://snoop.ro/pe-urmele-banilor-din-reclamele-rusesti-drumul-de-la-ministerul-apararii-din-rusia-la-date-personale-colectate-din-romania/', {
+    publicationDate: '2025-03-04',
+    evidence: 'Publisher article page displays “publicat în 04.03.2025”.',
+  }],
+  ['https://verstka.media/how-russians-earn-money-by-sending-each-other-to-war', {
+    publicationDate: '2025-12-17',
+    evidence: 'Publisher article header displays 17 December 2025.',
+  }],
+  ['https://www.theguardian.com/news/2024/mar/28/how-child-labour-in-india-makes-the-paving-stones-beneath-our-feet', {
+    publicationDate: '2024-03-28',
+    evidence: 'Publisher article byline displays Thu 28 Mar 2024; 9 May is marked as last modified.',
+  }],
+  ['https://panel.animalpolitico.com/corrupcion-detras-desabasto-medicamentos/', {
+    publicationDate: '2025-07-26',
+    evidence: 'Publisher’s special-project listing displays 26 July 2025 for this investigation.',
+  }],
+  ['https://www.facebook.com/anasglobal/videos/watch-galamsey-economy-video/858993851792503/', {
+    publicationDate: '2022-11-16',
+    evidence: 'Owner confirmed the Facebook video publication date as 16 November 2022.',
+  }],
   ['https://www.youtube.com/watch?v=fyGdDKA097E', {
     publishedAt: '2025-07-22T12:01:21.000Z',
     evidence: 'Original video meta itemprop=datePublished: 2025-07-22T05:01:21-07:00.',
